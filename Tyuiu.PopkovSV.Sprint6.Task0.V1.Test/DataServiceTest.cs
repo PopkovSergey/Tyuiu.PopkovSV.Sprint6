ac396ea24b73208e474c22c9414afca3ae6d2701
Tyuiu.PopkovSV <popkovsergej10@gmail.com>
2023-12-11 19:23:59 +0300

    Task0

---
+++ Tyuiu.PopkovSV.Sprint6.Task0.V1.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tyuiu.PopkovSV.Sprint6.Task0.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.PopkovSV.Sprint6.Task0.V1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PopkovSV.Sprint6.Task0.V1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PopkovSV.Sprint6.Task0.V1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a1f6bb07-2cac-40d9-9960-d2b4b0ceae1c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
