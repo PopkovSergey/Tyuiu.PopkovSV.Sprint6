5e9b972b5ecb77ff04285b2a509d5a09131a6d05
Tyuiu.PopkovSV <popkovsergej10@gmail.com>
2023-12-11 19:27:35 +0300

    Task1

---
+++ Tyuiu.PopkovSV.Sprint6.Task1.V5.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tyuiu.PopkovSV.Sprint6.Task1.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
