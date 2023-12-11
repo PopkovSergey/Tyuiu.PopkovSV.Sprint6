035c3c8a52673eed7877c0b88fdaf4a956be26cf
Tyuiu.PopkovSV <popkovsergej10@gmail.com>
2023-12-11 19:30:12 +0300

    Task2

---
+++ Tyuiu.PopkovSV.Sprint6.Task2.V12.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PopkovSV.Sprint6.Task2.V12.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PopkovSV.Sprint6.Task2.V12.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cbc500f6-b1c6-42b0-bd21-aca0b1ba1ea7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
